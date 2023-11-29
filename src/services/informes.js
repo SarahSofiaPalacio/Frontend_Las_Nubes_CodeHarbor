e7d9810ecc5b8f2a13917e54baf0c09437c5a3ba
Johan Acuña <johan.acuna@utp.edu.co>
2023-11-28 20:53:59 -0500

    Conexion informes

--- src/services/informes.js
+++ src/services/informes.js
@@ -4,24 +4,30 @@ import pdfMockURL from '../mocks/informes.pdf';
 const URL_BASE = process.env.REACT_APP_URL_BASE;
 const USE_MOCK = process.env.REACT_APP_USE_MOCK === "true";
 
-export const getReport = (reportType) => {
-    if (USE_MOCK) {
-      // Retornar una promesa que simula una llamada al servidor
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(pdfMockURL);
-        }, 1000);
-      });
-    } else {
-      // Realizar la llamada al backend y obtener un PDF
-      return axios({
-        url: `${URL_BASE}/informes/${reportType}`,
-        method: 'GET',
-        responseType: 'blob', // Importante para que axios trate la respuesta como un archivo binario
-      }).then(response => {
-        // Crear un URL para el Blob recibido
-        const fileURL = URL.createObjectURL(new Blob([response.data], { type: 'application/pdf' }));
-        return fileURL;
-      });
+export const getReport = async (token, reportType) => {
+  if (USE_MOCK) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(pdfMockURL);
+      }, 1000);
+    });
+  } else {
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      };
+      const response = await axios.get(`${URL_BASE}/users/informe/${reportType}`, config);
+      return response.data;
+    } catch (error) {
+      if (error.response) {
+        throw new Error(error.response.data.msg || 'Error desconocido');
+      } else if (error.request) {
+        throw new Error('No hay respuesta del servidor');
+      } else {
+        throw new Error('Error al configurar la petición');
+      }
     }
-  };
+  }
+};
--- src/views/Administrador/Informes.jsx
+++ src/views/Administrador/Informes.jsx
@@ -1,26 +1,25 @@
 import React, { useState } from 'react';
 import Header from '../../components/Header';
+import { useAuth } from '../../auth/AuthContext';
 import { getReport } from '../../services/informes.js';
 
 function Informes() {
-  // Estado para manejar si un informe está siendo generado
+  const { token } = useAuth();
   const [isGenerating, setIsGenerating] = useState(false);
 
   const handleDownloadReport = async (reportType) => {
-    console.log("Generando informe de %s...", reportType);
-    setIsGenerating(true); // Bloquea los botones mientras se genera el informe
-    getReport(reportType)
-      .then(response => {
-        console.log("Informe generado:", response);
-        window.open(response);
-        setIsGenerating(false); // Desbloquea los botones
-      })
-      .catch(error => {
-        console.error("Error al generar informe:", error);
-        setIsGenerating(false); // Desbloquea los botones
-      });
-  }
-
+    setIsGenerating(true);
+    try {
+      const pdfURL = await getReport(token, reportType);
+      console.log("(Informes) Informe de %s generado exitosamente", reportType);
+      window.open(pdfURL, '_blank');
+    } catch (error) {
+      console.log("(Informes) Error al generar informe de %s:", reportType, error);
+    } finally {
+      setIsGenerating(false);
+    }
+  };
+  
   return (
     <div>
       <Header
@@ -30,12 +29,12 @@ function Informes() {
 
       <div className="container my-5">
         <div className="row justify-content-center">
-          {/* Button 1 */}
+          {/* Button 1 Pacientes */}
           <div className="col-md-4 text-center mb-3">
             <button 
               type="button" 
               className="btn btn-primary w-75" 
-              onClick={() => handleDownloadReport('pacientes')}
+              onClick={() => handleDownloadReport(1)}
               disabled={isGenerating}
             >
               {/* Poner imagen en un recuadro con los bordes redondeado y con tamaño ajustado */}
@@ -43,24 +42,24 @@ function Informes() {
               Generar informes de Pacientes
             </button>
           </div>
-          {/* Button 2 */}
+          {/* Button 2 Colaboradores */}
           <div className="col-md-4 text-center mb-3">
             <button 
               type="button" 
               className="btn btn-primary w-75" 
-              onClick={() => handleDownloadReport('colaboradores')}
+              onClick={() => handleDownloadReport(2)}
               disabled={isGenerating}
             >
               <img src={`${process.env.PUBLIC_URL}/img/informe_colaboradores.svg`} alt="Informe de Colaboradores" className="img-fluid mb-2 mt-2 rounded mx-auto d-block "/>
               Generar informes de Colaboradores
             </button>
           </div>
-          {/* Button 3 */}
+          {/* Button 3 Medicamentos */}
           <div className="col-md-4 text-center mb-3">
             <button 
               type="button" 
               className="btn btn-primary w-75" 
-              onClick={() => handleDownloadReport('medicamentos')}
+              onClick={() => handleDownloadReport(3)}
               disabled={isGenerating}
             >
               <img src={`${process.env.PUBLIC_URL}/img/informe_medicamentos.svg`} alt="Informe de Medicamentos" className="img-fluid mb-2 mt-2 rounded mx-auto d-block "/>
